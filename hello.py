a77888a6a03e73c327409d4173c36ba92c817d1f
Nick Q <nick@localhost.localdomain>
2016-01-31 12:05:13 -0500

    Adding some IP returns

--- hello.py
+++ hello.py
@@ -1,32 +1,41 @@
 from flask import Flask
 from functools import wraps
 from flask import request, Response
+import commands
 
-def check_auth(uname, passwd):
-    return uname == 'admin' and passwd == 'admin'
+#def check_auth(uname, passwd):
+#    return uname == 'admin' and passwd == 'admin'
 
-def authenticate():
-    return Response('not authorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+#def authenticate():
+#    return Response('not authorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
+#def requires_auth(f):
+#    @wraps(f)
+#    def decorated(*args, **kwargs):
+#        auth = request.authorization
+#        if not auth or not check_auth(auth.username, auth.password):
+#            return authenticate()
+#        return f(*args, **kwargs)
+#    return decorated
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hellow World, from Nick"
+    return "Hello World, from Nick"
 
-@app.route("/secure")
-@requires_auth
-def sec():
-    return "I'm secure"
+@app.route("/ip")
+def get_ip():
+    return commands.getoutput('ifconfig | grep inet')
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/gateway")
+def get_gateway():
+    return commands.getoutput('netstat -nr')
+
+#@app.route("/secure")
+#@requires_auth
+#def sec():
+#    return "I'm secure"
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
